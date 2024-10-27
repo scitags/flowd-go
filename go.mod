c040288a353cf27358c808d639f9fac7b8bef6f1
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-10-27 20:13:28 +0100

    Add FlowState type and interfaces

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.23.0
 
 require (
 	github.com/aquasecurity/libbpfgo v0.7.0-libbpf-1.4.0.20240729111821-61d531acf4ca
+	github.com/rjeczalik/notify v0.9.3
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 )
--- types.go
+++ types.go
@@ -2,11 +2,12 @@ package glowd
 
 import (
 	"net"
+	"strings"
 	"time"
 )
 
 type FlowID struct {
-	State      string
+	State      FlowState
 	Protocol   Protocol
 	Src        IPPort
 	Dst        IPPort
@@ -19,7 +20,7 @@ type FlowID struct {
 
 type IPPort struct {
 	IP   net.IP
-	Port int
+	Port uint16
 }
 
 type Protocol int
@@ -29,5 +30,63 @@ const (
 	UDP
 )
 
-type foo interface {
+// TODO: Find a way to make this nicer. Maybe init() functions is the
+// TODO: way to go?
+var (
+	protocolMap = map[string]Protocol{
+		"TCP": TCP,
+		"UDP": UDP,
+	}
+
+	locotorpMap = map[Protocol]string{
+		TCP: "TCP",
+		UDP: "UDP",
+	}
+
+	flowMap = map[string]FlowState{
+		"START": START,
+		"END":   END,
+	}
+
+	wolfMap = map[FlowState]string{
+		START: "START",
+		END:   "END",
+	}
+)
+
+func (p Protocol) String() string {
+	return locotorpMap[p]
+}
+
+func ParseProtocol(proto string) (Protocol, bool) {
+	p, ok := protocolMap[strings.ToUpper(proto)]
+	return p, ok
+}
+
+type FlowState int
+
+const (
+	START FlowState = iota
+	END
+)
+
+func (fs FlowState) String() string {
+	return wolfMap[fs]
+}
+
+func ParseFlowState(flowState string) (FlowState, bool) {
+	fs, ok := flowMap[strings.ToUpper(flowState)]
+	return fs, ok
+}
+
+type Backend interface {
+	Init() error
+	Run(<-chan struct{}, <-chan FlowID)
+	Cleanup() error
+}
+
+type Plugin interface {
+	Init() error
+	Run(<-chan struct{}, chan<- FlowID)
+	Cleanup() error
 }
