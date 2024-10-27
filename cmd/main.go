5966e48d82251274a1a4d377dbb8b6ffa2a3e76a
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-10-27 20:13:59 +0100

    Add np plugin test

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
+	"os/signal"
 
+	"github.com/pcolladosoto/glowd"
 	"github.com/pcolladosoto/glowd/backends/ebpf"
+	"github.com/pcolladosoto/glowd/plugins/np"
+
 	"github.com/spf13/cobra"
 )
 
@@ -31,7 +36,45 @@ var (
 		Use:   "ebpf-test",
 		Short: "Try to load the eBPF program.",
 		Run: func(cmd *cobra.Command, args []string) {
-			ebpf.Launch()
+			ebpf.Init()
+		},
+	}
+
+	npTest = &cobra.Command{
+		Use:   "np-test",
+		Short: "Try to create and read from a named pipe.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := np.Init(); err != nil {
+				fmt.Printf("error setting up the named pipe: %v\n", err)
+				os.Exit(-1)
+			}
+			defer func() {
+				if err := np.Cleanup(); err != nil {
+					fmt.Printf("error cleaning up the named pipe: %v\n", err)
+				}
+			}()
+
+			sigChan := make(chan os.Signal, 1)
+			signal.Notify(sigChan, os.Interrupt)
+
+			flowChan := make(chan glowd.FlowID)
+			doneChan := make(chan struct{})
+			go np.Run(doneChan, flowChan)
+
+			for {
+				select {
+				case flowID, ok := <-flowChan:
+					if !ok {
+						fmt.Printf("flowChannel closed by producer...\n")
+						return
+					}
+					fmt.Printf("got a flow: %+v\n", flowID)
+				case <-sigChan:
+					close(doneChan)
+					return
+				}
+			}
+
 		},
 	}
 
@@ -46,9 +89,13 @@ func init() {
 	// Add the different sub-commands
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(ebpfTest)
+	rootCmd.AddCommand(npTest)
 }
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	slog.SetDefault(logger)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
