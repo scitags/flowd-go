4b6d40519cc92c17a184d2b8df3ec642d00fb7a7
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-11-04 16:41:46 +0100

    Fix Darwin-based backend

--- backends/ebpf/stub.go
+++ backends/ebpf/stub.go
@@ -4,7 +4,14 @@ package ebpf
 
 import "github.com/pcolladosoto/glowd"
 
+type EbpfBackend struct {
+}
+
+func New() *EbpfBackend {
+	return &EbpfBackend{}
+}
+
 // Just implement the glowd.Backend interface
-func Init() error                              { return nil }
-func Run(<-chan struct{}, <-chan glowd.FlowID) {}
-func Cleanup() error                           { return nil }
+func (b *EbpfBackend) Init() error                              { return nil }
+func (b *EbpfBackend) Run(<-chan struct{}, <-chan glowd.FlowID) {}
+func (b *EbpfBackend) Cleanup() error                           { return nil }
