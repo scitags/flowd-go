b59f134083c7c89b50cbce4893e7a85f23a8f9cf
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-11-07 10:58:49 +0100

    Fix JSON tag

--- backends/firefly/firefly.go
+++ backends/firefly/firefly.go
@@ -82,7 +82,7 @@ var (
 )
 
 type FireflyBackendConf struct {
-	FireflyDestinationPort uint16 `json:"FireflyDestinationPort"`
+	FireflyDestinationPort uint16 `json:"fireflyDestinationPort"`
 	PrependSyslog          bool   `json:"prependSyslog"`
 }
 
