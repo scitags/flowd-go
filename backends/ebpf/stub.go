4e073d83ae869f9d184b98a1d6491115f525c5b5
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-11-07 13:56:26 +0100

    Fix compilation on macOS

--- backends/ebpf/stub.go
+++ backends/ebpf/stub.go
@@ -2,16 +2,33 @@
 
 package ebpf
 
-import "github.com/pcolladosoto/glowd"
+import (
+	"github.com/pcolladosoto/glowd"
+)
 
 type EbpfBackend struct {
 }
 
-func New() *EbpfBackend {
+type EbpfBackendConf struct {
+}
+
+func (c *EbpfBackendConf) UnmarshalJSON(data []byte) error {
+	*c = EbpfBackendConf{}
+	return nil
+}
+
+func New(conf *EbpfBackendConf) *EbpfBackend {
 	return &EbpfBackend{}
 }
 
 // Just implement the glowd.Backend interface
-func (b *EbpfBackend) Init() error                              { return nil }
-func (b *EbpfBackend) Run(<-chan struct{}, <-chan glowd.FlowID) {}
-func (b *EbpfBackend) Cleanup() error                           { return nil }
+func (b *EbpfBackend) Init() error {
+	return nil
+}
+
+func (b *EbpfBackend) Run(<-chan struct{}, <-chan glowd.FlowID) {
+}
+
+func (b *EbpfBackend) Cleanup() error {
+	return nil
+}
