491197479bf3223ff32bd7d514b9cf728f7dc8e3
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-11-13 19:01:18 +0100

    Add tentative HBH + DO implementation

--- backends/ebpf/progs/marker.bpf.h
+++ backends/ebpf/progs/marker.bpf.h
@@ -5,6 +5,7 @@
 // conflicts with the inclusion of vmlinux.h!
 #define TC_ACT_UNSPEC (-1)
 #define TC_ACT_OK 0
+#define TC_ACT_SHOT 2
 
 // The ETH_P_* constants are defined in include/uapi/linux/if_ether.h
 // (i.e. /usr/include/linux/if_ether.h). Again, their inclusion conflicts
@@ -19,8 +20,11 @@
 #define PROTO_UDP       0x11
 #define PROTO_IPV6_ICMP 0x3A
 
-// Check RFC 2460 Section 4.3: https://www.rfc-editor.org/rfc/rfc2460.html#section-4.3
+// Check RFC 2460 Section 4.3 and 4.6:
+//   https://www.rfc-editor.org/rfc/rfc2460.html#section-4.3
+//   https://www.rfc-editor.org/rfc/rfc2460.html#section-4.6
 #define NEXT_HDR_HOP_BY_HOP 0x0
+#define NEXT_HDR_DEST_OPTS 60
 
 // The keys for our hash maps. Should we maybe combine the ports into a __u32?
 struct fourTuple {
@@ -45,4 +49,13 @@ struct hopByHopHdr_t {
 	__u8 nextHdr;
 	__u8 hdrLen;
 	__u8 opts[6];
+};
+
+struct hopByHopAndDestOptsHdr_t {
+	__u8 hbhNextHdr;
+	__u8 hbhHdrLen;
+	__u8 optsHbH[6];
+	__u8 destNextHdr;
+	__u8 destHdrLen;
+	__u8 optsDest[6];
 };
