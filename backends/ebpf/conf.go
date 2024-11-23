b1adf6f7926ffc1dfa829e56ea5d44b1ca365a2d
Pablo Collado Soto <pcolladosoto@gmx.com>
2024-11-23 21:42:43 +0100

    Refactor de eBPF backend

--- backends/ebpf/conf.go
+++ backends/ebpf/conf.go
@@ -3,34 +3,24 @@
 package ebpf
 
 import (
-	"encoding/json"
-	"fmt"
 	"strings"
 )
 
-type MarkingStrategy int
+type MarkingStrategy string
 
 const (
-	FlowLabelMarking MarkingStrategy = iota
-	HopByHopHeaderMarking
-	HopByHopDestHeadersMarking
+	FlowLabelMarking           MarkingStrategy = "flowLabel"
+	HopByHopHeaderMarking      MarkingStrategy = "hopByHop"
+	HopByHopDestHeadersMarking MarkingStrategy = "hopByHopAndDestination"
 )
 
 var (
-	configurationTags = map[string]bool{
-		strings.ToLower("targetInterface"): false,
-		strings.ToLower("removeQdisc"):     false,
-		strings.ToLower("programPath"):     false,
-		strings.ToLower("markingStrategy"): false,
-		strings.ToLower("debugMode"):       false,
-	}
-
-	DefaultConf = EbpfBackendConf{
-		TargetInterface: "lo",
-		RemoveQdisc:     true,
-		ProgramPath:     "",
-		MarkingStrategy: FlowLabelMarking,
-		DebugMode:       false,
+	Defaults = map[string]interface{}{
+		"targetInterface": "lo",
+		"RemoveQdisc":     true,
+		"programPath":     "",
+		"markingStrategy": FlowLabelMarking,
+		"debugMode":       false,
 	}
 
 	markingStrategyMap = map[string]MarkingStrategy{
@@ -40,74 +30,6 @@ var (
 	}
 )
 
-type EbpfBackendConf struct {
-	TargetInterface string          `json:"targetInterface"`
-	RemoveQdisc     bool            `json:"removeQdisc"`
-	ProgramPath     string          `json:"programPath"`
-	MarkingStrategy MarkingStrategy `json:"-"`
-	DebugMode       bool            `json:"debugMode"`
-}
-
 type ebpfBackendMarkingConf struct {
 	MarkingStrategy string `json:"markingStrategy"`
-}
-
-// We need an alias to avoid infinite recursion
-// in the unmarshalling logic
-type AuxEbpfBackendConf EbpfBackendConf
-
-func (c *EbpfBackendConf) UnmarshalJSON(data []byte) error {
-	tmp := map[string]interface{}{}
-	if err := json.Unmarshal(data, &tmp); err != nil {
-		return fmt.Errorf("couldn't unmarshall into tmp map: %w", err)
-	}
-
-	for k := range tmp {
-		delete(configurationTags, strings.ToLower(k))
-	}
-
-	tmpConf := AuxEbpfBackendConf{}
-	if err := json.Unmarshal(data, &tmpConf); err != nil {
-		return fmt.Errorf("couldn't unmarshall into tmpConf: %w", err)
-	}
-
-	tmpMarkingStrategy := ebpfBackendMarkingConf{}
-	if err := json.Unmarshal(data, &tmpMarkingStrategy); err != nil {
-		return fmt.Errorf("couldn't unmarshall into tmpMarkingStrategy: %w", err)
-	}
-	markingStrategy, ok := markingStrategyMap[strings.ToLower(tmpMarkingStrategy.MarkingStrategy)]
-	if !ok && tmpMarkingStrategy.MarkingStrategy != "" {
-		return fmt.Errorf("wrong marking strategy %s, available ones are %v", tmpMarkingStrategy.MarkingStrategy,
-			func() []string {
-				markingStrategies := []string{}
-				for k := range markingStrategyMap {
-					markingStrategies = append(markingStrategies, k)
-				}
-				return markingStrategies
-			}(),
-		)
-	}
-	tmpConf.MarkingStrategy = markingStrategy
-
-	for k := range configurationTags {
-		switch strings.ToLower(k) {
-		case "targetinterface":
-			tmpConf.TargetInterface = DefaultConf.TargetInterface
-		case "removeqdisc":
-			tmpConf.RemoveQdisc = DefaultConf.RemoveQdisc
-		case "programpath":
-			tmpConf.ProgramPath = DefaultConf.ProgramPath
-		case "markingstrategy":
-			tmpConf.MarkingStrategy = DefaultConf.MarkingStrategy
-		case "debugmode":
-			tmpConf.DebugMode = DefaultConf.DebugMode
-		default:
-			return fmt.Errorf("unknown configuration key %q", k)
-		}
-	}
-
-	// Store the results!
-	*c = EbpfBackendConf(tmpConf)
-
-	return nil
 }
