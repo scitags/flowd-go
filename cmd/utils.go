e4f1fa216515e72bfeec75ffffcb6a43531d045f
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-01-29 18:47:44 +0100

    Relocate log formatting

--- cmd/utils.go
+++ cmd/utils.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"net"
+	"path/filepath"
 
+	glowd "github.com/scitags/flowd-go"
+	"github.com/scitags/flowd-go/backends/ebpf"
 	glowdTypes "github.com/scitags/flowd-go/types"
 )
 
@@ -39,4 +43,56 @@ func cleanupBackends(backends []glowdTypes.Backend) {
 			slog.Error("error cleaning up backend", "backend", backend, "err", err)
 		}
 	}
+}
+
+func logReplacements(groups []string, a slog.Attr) slog.Attr {
+	// Remove time.
+	if a.Key == slog.TimeKey && len(groups) == 0 {
+		return slog.Attr{}
+	}
+
+	// Remove the directory from the source's filename.
+	if a.Key == slog.SourceKey {
+		source := a.Value.Any().(*slog.Source)
+		source.File = filepath.Base(source.File)
+	}
+
+	// Format the flow tag as both a binary and hex number
+	if a.Key == glowd.FlowTagKey {
+		// When slog gobbles the flow tag it becomes a uint64 instead of a uint32
+		// apparently...
+		flowLabel, ok := a.Value.Any().(uint64)
+		if ok {
+			return slog.Attr{Key: a.Key, Value: slog.StringValue(fmt.Sprintf("%#x;(%#020b)", flowLabel, flowLabel))}
+		}
+	}
+
+	// Format the flow hashes
+	if a.Key == glowd.FlowHashKey {
+		flowHash, ok := a.Value.Any().(ebpf.FlowFourTuple)
+		if ok {
+			return slog.Attr{Key: a.Key, Value: slog.StringValue(
+				fmt.Sprintf("%s(%#x|%#x);%d;%d", net.IP([]byte{
+					byte(flowHash.IPv6Hi & (0xFF << 7) >> 7),
+					byte(flowHash.IPv6Hi & (0xFF << 6) >> 6),
+					byte(flowHash.IPv6Hi & (0xFF << 5) >> 5),
+					byte(flowHash.IPv6Hi & (0xFF << 4) >> 4),
+					byte(flowHash.IPv6Hi & (0xFF << 3) >> 3),
+					byte(flowHash.IPv6Hi & (0xFF << 2) >> 2),
+					byte(flowHash.IPv6Hi & (0xFF << 1) >> 1),
+					byte(flowHash.IPv6Hi & 0xFF),
+					byte(flowHash.IPv6Lo & (0xFF << 7) >> 7),
+					byte(flowHash.IPv6Lo & (0xFF << 6) >> 6),
+					byte(flowHash.IPv6Lo & (0xFF << 5) >> 5),
+					byte(flowHash.IPv6Lo & (0xFF << 4) >> 4),
+					byte(flowHash.IPv6Lo & (0xFF << 3) >> 3),
+					byte(flowHash.IPv6Lo & (0xFF << 2) >> 2),
+					byte(flowHash.IPv6Lo & (0xFF << 1) >> 1),
+					byte(flowHash.IPv6Lo & 0xFF),
+				}), flowHash.IPv6Hi, flowHash.IPv6Lo, flowHash.SrcPort, flowHash.DstPort),
+			)}
+		}
+	}
+
+	return a
 }
