b8b0f5570b93dc466dd1b340ce7b60dc150c48e0
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-02-13 09:23:37 +0100

    Add BPF_PROG_TYPE_SOCK_OPS program monitoring TCP
    It looks like we can get access to a `struct tpc_sock`
    from which we can gather all the information we can
    possibly need!

---
+++ enrichment/skops/cgroup_test.go
@@ -0,0 +1,37 @@
+package skops
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func init() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Remove time.
+			if a.Key == slog.TimeKey && len(groups) == 0 {
+				return slog.Attr{}
+			}
+			// Remove the directory from the source's filename.
+			if a.Key == slog.SourceKey {
+				source := a.Value.Any().(*slog.Source)
+				source.File = filepath.Base(source.File)
+			}
+			return a
+		},
+	}))
+	slog.SetDefault(logger)
+}
+
+func TestGetCgroupInfo(t *testing.T) {
+	path, err := GetCgroupInfo()
+	if err != nil {
+		t.Errorf("error getting cgroup info: %v\n", err)
+	}
+	fmt.Printf("running on cgroup %s\n", path)
+}
---
+++ enrichment/skops/ebpf_test.go
@@ -0,0 +1,34 @@
+package skops
+
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func init() {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Remove time.
+			if a.Key == slog.TimeKey && len(groups) == 0 {
+				return slog.Attr{}
+			}
+			// Remove the directory from the source's filename.
+			if a.Key == slog.SourceKey {
+				source := a.Value.Any().(*slog.Source)
+				source.File = filepath.Base(source.File)
+			}
+			return a
+		},
+	}))
+	slog.SetDefault(logger)
+}
+
+func TestEbpfAttach(t *testing.T) {
+	if err := Init(); err != nil {
+		t.Errorf("error attaching the program: %v\n", err)
+	}
+}
