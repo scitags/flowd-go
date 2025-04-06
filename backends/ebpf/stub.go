39c720bc87d8ed0aadb10cd7b4ef142e1db45029
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-04-06 21:25:21 +0200

    Do not bundle vmlinux.h in the SRPM

--- backends/ebpf/stub.go
+++ backends/ebpf/stub.go
@@ -29,3 +29,13 @@ func (b *EbpfBackend) Run(<-chan struct{}, <-chan glowdTypes.FlowID) {
 func (b *EbpfBackend) Cleanup() error {
 	return nil
 }
+
+// This type definition is needed so that the cmd utils won't
+// complain. Should we maybe relocate this definition someplace
+// else?
+type FlowFourTuple struct {
+	IPv6Hi  uint64
+	IPv6Lo  uint64
+	DstPort uint16
+	SrcPort uint16
+}
