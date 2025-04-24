25f9e94465ed1374c4b371cdf0de5322ed471bbe
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-04-24 16:58:09 +0200

    Streamline the source bundling process

--- mk/rpm.mk
+++ mk/rpm.mk
@@ -13,6 +13,8 @@
 #   https://docs.fedoraproject.org/en-US/packaging-guidelines/Golang/
 #   https://rpm-software-management.github.io/mock/
 #   https://rpm-packaging-guide.github.io
+#   https://gitlab.cern.ch/linuxsupport/myrpm
+#   https://gitlab.cern.ch/linuxsupport/rpmci
 
 SPECFILE             = $(shell find . -type f -name *.spec)
 SPECFILE_NAME        = $(shell awk '$$1 == "Name:"     { print $$2 }' $(SPECFILE))
@@ -20,7 +22,20 @@ SPECFILE_VERSION     = $(shell awk '$$1 == "Version:"  { print $$2 }' $(SPECFILE
 SPECFILE_RELEASE     = $(shell awk '$$1 == "Release:"  { print $$2 }' $(SPECFILE))
 DIST                ?= $(shell rpm --eval %{dist})
 
+# How are we going to bundle the sources into a *.tar.gz? By default we'll leverage Go
+# to vendor and generate the Makefile, but we can also download a ready-made copy from
+# a preexisting GitHub release and call it a day... In order to use this second mode
+# one just needs to specify anything other than 'go' when invoking the sources
+# target, for instance: make SRC_GEN_MODE=curl
+SRC_GEN_MODE ?= go
+
+# Let's get the Go version we're currently using so that we can download it should we
+# need to.
 GO_VERSION = $(shell awk '/^go[[:space:]]/ {print $$2}' go.mod)
+
+# The name of the *.tar.gz with the bundled sources. Note this name MUST be the one specified
+# in the SPEC file on field Sources0.
+SOURCES_FILENAME := ${SPECFILE_NAME}-${SPECFILE_VERSION}.tar.gz
 
 # Path of the buildroot created with rpmdev-setuptree. In order to create
 # it, install the necessary tools as explained on the main README.md and
@@ -28,12 +43,24 @@ GO_VERSION = $(shell awk '/^go[[:space:]]/ {print $$2}' go.mod)
 # default on ${HOME}/rpmbuild, hence the definition of this variable.
 RPM_BUILDROOT = $(shell echo ${HOME})/rpmbuild
 
+# If we have to download either Go or jq we need to know the architecture we're running on.
+# The value returned by uname(1) is not the one used when distributing software, so we'll
+# just do a naive translation. We could even safely assume to always be running on amd64
+# though....
 ARCH := $(shell uname -m)
 ifeq ($(ARCH),x86_64)
 DL_ARCH := amd64
 else
 DL_ARCH := arm64
 endif
+
+# Everything jq-related. This URL we'll be leveraged if running in the download-a-ready-made-file mode.
+JQ_VERSION      := 1.7.1
+JQ_DOWNLOAD_URL := https://github.com/jqlang/jq/releases/download/jq-$(JQ_VERSION)/jq-linux-$(DL_ARCH)
+
+# The URLs from GitHub's API allowing us to get the latest release and download the read-made sources.
+GH_LATEST_RELEASE_URL   := https://github.com/scitags/flowd-go/releases/latest
+GH_SOURCES_DOWNLOAD_URL := https://github.com/scitags/flowd-go/releases/download/$(RELEASE_VER)/sources.tar.gz
 
 # Simply show the variables we'll use in the build
 .PHONY: rpm-dbg
@@ -48,7 +75,7 @@ rpm-dbg:
 	@echo "      GO_VERSION: $(GO_VERSION)"
 
 # Files to include in the SRPM
-RPM_FILES := backends cmd enrichment plugins settings rpm stun types const.go go.mod go.sum vendor Makefile
+RPM_FILES := backends cmd enrichment plugins settings rpm stun types const.go go.mod go.sum Makefile vendor commit
 
 # This target will bundle all the source files so that we can easily create a SRPM with our SPEC file.
 # We'll also bundle vmlinux.h so as to avoid having to generate that on the machine we might generate
@@ -58,60 +85,50 @@ RPM_FILES := backends cmd enrichment plugins settings rpm stun types const.go go
 # are actually deleting the .git directory!). Please note the target name MUST be sources as this is
 # what CERN's koji instance expects! Be sure to check https://gitlab.cern.ch/linuxsupport/myrpm for
 # a great working example. The repo at https://gitlab.cern.ch/linuxsupport/rpmci is also a fundamental
-# source of information. If we are to generate the SRPM and upload it as a GitHub release artifact we
-# can leverage the following:
-#     curl -L -o jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-$(DL_ARCH)
-#     chmod +x jq
-#     RELEASE_VER := $(shell curl -L -s -H 'Accept: application/json' https://github.com/scitags/flowd-go/releases/latest | jq -r .tag_name)
-#     curl -L -o build/SRPMS/flowd-go-$(SPECFILE_VERSION)-$(SPECFILE_RELEASE).src.rpm https://github.com/scitags/flowd-go/releases/download/$(RELEASE_VER)/flowd-go.src.rpm
-# An even better approach could be to simply download the *.tar.gz and call it a day with:
-#     curl -L -o jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-$(DL_ARCH)
-#     chmod +x jq
-#     RELEASE_VER := $(shell curl -L -s -H 'Accept: application/json' https://github.com/scitags/flowd-go/releases/latest | jq -r .tag_name)
-#     curl -L -o ${SPECFILE_NAME}-${SPECFILE_VERSION}.tar.gz https://github.com/scitags/flowd-go/releases/download/$(RELEASE_VER)/sources.tar.gz
+# source of information.
 .PHONY: sources
-sources: rpm-clean
+sources:
+	mkdir -p $(PWD)/build
+
 	ls -la
 
-	mkdir -p $(PWD)/dist/${SPECFILE_NAME}-${SPECFILE_VERSION} $(PWD)/build
-
-	@# Only download Go if it's not present on the PATH
+ifeq ($(SRC_GEN_MODE),go)
+	@# Only download Go if it's not present on the PATH.
 	which go || curl -L -s -o go.tar.gz https://dl.google.com/go/go$(GO_VERSION).linux-$(DL_ARCH).tar.gz
 	which go || tar -C /tmp -xzf go.tar.gz
 	which go || /tmp/go/bin/go version
 
-	@# Try to use a 'normally' installed Go release and, if not, use the one we should've already installed...
+	@# Try to use a 'normally' installed Go release and, if not, use the one we should've already installed.
 	go mod vendor || /tmp/go/bin/go mod vendor
 	go generate   || /tmp/go/bin/go generate
 	gzip --force rpm/$(SPECFILE_NAME).1
 
-	cp -pr ${RPM_FILES} dist/${SPECFILE_NAME}-${SPECFILE_VERSION}/.
-	cat .git/$(shell cut -d ' ' -f 2 .git/HEAD)
-	cat .git/$(shell cut -d ' ' -f 2 .git/HEAD) > dist/${SPECFILE_NAME}-${SPECFILE_VERSION}/commit
+	@# Record the current commit so that it can be embedded in the resulting binary.
+	cat .git/$(shell cut -d ' ' -f 2 .git/HEAD) > commit
 
-	find dist -type d -name .git       | xargs -i rm -rf {}
-	find dist -type d -name '*sample*' | xargs -i rm -rf {}
-	find dist -type f -name '*.gdb'    | xargs -i rm -rf {}
-	find dist -type f -name '*.pcapng' | xargs -i rm -rf {}
-	find dist -type f -name '*.o'      | xargs -i rm -rf {}
-	find dist -type f -name 'test'     | xargs -i rm -rf {}
+	@# Simply append the needed prefix to every entry in the *.tar.gz instead of creating a tree.
+	tar -czvf $(SOURCES_FILENAME) --transform 's,^,${SPECFILE_NAME}-${SPECFILE_VERSION}/,' $(RPM_FILES)
 
-	cd dist; tar -czvf ${SPECFILE_NAME}-${SPECFILE_VERSION}.tar.gz ${SPECFILE_NAME}-${SPECFILE_VERSION}
-
-	cp dist/${SPECFILE_NAME}-${SPECFILE_VERSION}.tar.gz .
+	@# Make a copy of the sources with a known name to include it in GitHub releases. This could be done
+	@# on the GitHub CI workflows, but it's much easier to do it here instead.
+	cp $(SOURCES_FILENAME) sources.tar.gz
+else
+	curl -sLo jq $(JQ_DOWNLOAD_URL); chmod +x jq
+	$(eval RELEASE_VER := $(shell curl -L -s -H 'Accept: application/json' $(GH_LATEST_RELEASE_URL) | ./jq -r .tag_name))
+	curl -sLo $(SOURCES_FILENAME) $(GH_SOURCES_DOWNLOAD_URL)
+endif
 
 # Simply build a SRPM after bundling the sources. Please note the target name MUST be srpm as this is what CERN's koji
 # instance expects.
 .PHONY: srpm
 srpm: sources
-	ls -la
-	rpmbuild -bs --define "dist $(DIST)" --define "_topdir $(PWD)/build" --define '_sourcedir $(PWD)/dist' $(SPECFILE)
+	rpmbuild -bs --define "dist $(DIST)" --define "_topdir $(PWD)/build" --define '_sourcedir $(PWD)' $(SPECFILE)
 
 # Build the binary (i.e. carrying teh compiled binary) RPM. Please note the target name MUST be rpm as this is what
 # CERN's koji instance expects.
 .PHONY: rpm
 rpm: sources
-	rpmbuild -bb --define "dist $(DIST)" --define "_topdir $(PWD)/build" --define '_sourcedir $(PWD)/dist' $(SPECFILE)
+	rpmbuild -bb --define "dist $(DIST)" --define "_topdir $(PWD)/build" --define '_sourcedir $(PWD)' $(SPECFILE)
 
 # Note how we need network access so that Go can pull its dependencies!
 .PHONY: rpm-mock
@@ -124,4 +141,4 @@ rpm-mock: srpm
 
 .PHONY: rpm-clean
 rpm-clean:
-	rm -rf build dist vendor
+	rm -rf build dist vendor *.tar.gz commit
