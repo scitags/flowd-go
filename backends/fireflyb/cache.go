e2ff06dab780db57ea20be38727e1dd45ffe93e5
Pablo Collado Soto <pcolladosoto@gmx.com>
2025-05-07 10:26:49 +0200

    Refactor de firefly backend

--- backends/fireflyb/cache.go
+++ backends/fireflyb/cache.go
@@ -1,9 +1,11 @@
 package fireflyb
 
 import (
+	"log/slog"
 	"sync"
 
 	"github.com/scitags/flowd-go/enrichment/netlink"
+	glowdTypes "github.com/scitags/flowd-go/types"
 )
 
 type connectionCache struct {
@@ -39,4 +41,18 @@ func (cc *connectionCache) Remove(key uint64) {
 	cc.Lock()
 	delete(cc.cache, key)
 	cc.Unlock()
+}
+
+func (b *FireflyBackend) hashFlowID(flowID glowdTypes.FlowID) uint64 {
+	// Encoding a flowID will never fail!
+	enc, _ := flowID.MarshalBinary()
+
+	b.hashGen.Reset()
+	b.hashGen.Write(enc)
+
+	hash := b.hashGen.Sum64()
+
+	slog.Debug("hashed flowID", "hash", hash)
+
+	return hash
 }
